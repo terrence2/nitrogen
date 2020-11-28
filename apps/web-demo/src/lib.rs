3e545c31e0c520dc0f6afe3da0a0c79462379e16
Terrence Cole <terrence.d.cole@gmail.com>
2020-11-28 08:15:53 -0800

    Framework for compiling for the web.

---
+++ apps/web-demo/src/lib.rs
@@ -0,0 +1,57 @@
+// This file is part of Nitrogen.
+//
+// Nitrogen is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Nitrogen is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Nitrogen.  If not, see <http://www.gnu.org/licenses/>.
+use command::Bindings;
+use failure::{bail, Fallible};
+use input::{InputController, InputSystem};
+use wasm_bindgen::prelude::*;
+use web_sys::console;
+use winit::window::Window;
+
+#[wasm_bindgen]
+pub fn wasm_main() {
+    console::log_1(&"BEFORE MAIN".into());
+    main().ok();
+    console::log_1(&"AFTER MAIN".into());
+}
+
+fn main() -> Fallible<()> {
+    let system_bindings = Bindings::new("map")
+        .bind("demo.bail", "b")?
+        .bind("demo.panic", "p")?
+        .bind("demo.exit", "Escape")?
+        .bind("demo.exit", "q")?;
+    InputSystem::run_forever(vec![system_bindings], window_main)
+}
+
+fn window_main(
+    window: Window,
+    input_controller: InputController,
+) -> Fallible<Box<dyn FnMut() -> Fallible<()> + 'static>> {
+    return Ok(Box::new(move || {
+        for command in input_controller.poll()? {
+            println!("COMMAND: {:?} <- {:?}", window, command);
+            match command.command() {
+                "exit" => {
+                    input_controller.quit()?;
+                    return Ok(());
+                }
+                "bail" => bail!("soft crash"),
+                "panic" => bail!("hard panic"),
+                _ => {}
+            }
+        }
+        Ok(())
+    }));
+}
