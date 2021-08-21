4745e4e61ca9cef7ec0740297aa0131d21f9fc85
Terrence Cole <terrence.d.cole@gmail.com>
2021-08-21 13:54:30 -0700

    Running headless tests

--- shell.nix
+++ shell.nix
@@ -8,5 +8,11 @@
       pkgs.rust-bin.stable.latest.default
       pkgs.gnumake
       pkgs.gmock
+      pkgs.glxinfo
+      pkgs.vulkan-tools
+      pkgs.xorg.libX11
+      pkgs.xorg.libXrandr
     ];
+    LD_LIBRARY_PATH = with pkgs.xlibs; "${pkgs.vulkan-loader}/lib:${pkgs.mesa}/lib:${libX11}/lib:${libXcursor}/lib:${libXxf86vm}/lib:${libXi}/lib:${libXrandr}/lib";
+    DISPLAY = ":0";
   }
